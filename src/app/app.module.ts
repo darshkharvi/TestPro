1dd8b0b32b790db6ecb570770283b88707662bc5
darshkharvi <darshkharvidash@gmail.com>
2025-11-11 12:19:15 +0530

    designation changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DesignationComponent } from './modules/designation/designation.component';
@@ -14,7 +15,9 @@ import { DepartmentComponent } from './modules/department/department.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/designation/designation.component.ts
+++ src/app/modules/designation/designation.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DesignationModel } from 'src/app/models/designationModel';
 
 @Component({
   selector: 'app-designation',
   templateUrl: './designation.component.html',
   styleUrls: ['./designation.component.css']
 })
-export class DesignationComponent {
-
+export class DesignationComponent implements OnInit {
+ 
+  designationForm!: FormGroup;
+  designations: DesignationModel[] = [];
+ 
+  constructor(
+    private fb: FormBuilder,
+    // private designationService: DesignationService
+  ) {}
+ 
+  ngOnInit(): void {
+    this.designationForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['']
+    });
+ 
+    // Load existing designations
+    // this.designations = this.designationService.getDesignations();
+  }
+ 
+  onSubmit(): void {
+    if (this.designationForm.valid) {
+      const newDesignation: DesignationModel = this.designationForm.value;
+      // this.designationService.addDesignation(newDesignation);
+      // this.designations = this.designationService.getDesignations();
+ 
+      console.log('Form Data:', this.designationForm.value);
+      this.designationForm.reset();
+    } else {
+      this.designationForm.markAllAsTouched();
+    }
+  }
 }
