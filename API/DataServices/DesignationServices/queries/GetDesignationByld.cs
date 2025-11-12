e7dfeeff24bbfee069a49a474e657fef6800a796
darshkharvi <darshkharvidash@gmail.com>
2025-11-12 11:17:20 +0530

    updated

--- API/DataServices/DesignationServices/queries/GetDesignationByld.cs
+++ API/DataServices/DesignationServices/queries/GetDesignationByld.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using DataCore;
+using DataModels;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace DataServices.DesignationServices.queries
 {
-  internal class GetDesignationByld
+  public class GetDesignationById : IRequest<DesignationModel>
   {
+    public int Id { get; set; }
+  }
+  public class GetDesignationByIdHandler : IRequestHandler<GetDesignationById, DesignationModel>
+  {
+    private readonly ITestProUnitOfWork _context;
+    public GetDesignationByIdHandler(ITestProUnitOfWork context)
+    {
+      _context = context;
+    }
+    public async Task<DesignationModel> Handle(GetDesignationById request, CancellationToken cancellationToken)
+    {
+      try
+      {
+        var data = _context.Repository<DataEntities.Designation>()
+            .Get()
+            .Where(c => c.Id == request.Id)
+            .FirstOrDefault();
+
+        if (data != null)
+        {
+          return new DesignationModel
+          {
+            Id = data.Id,
+            Title = data.Title,
+            Description = data.Description,
+          };
+        }
+        return null;
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
   }
 }
